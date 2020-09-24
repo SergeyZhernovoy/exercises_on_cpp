28aa434a8aa3ec11b07a095c7df1c20b042df858
s.zhernovoy <s_jernov@mail.ru>
2020-09-25 00:26:02 +0300

    fix#1

--- Helpers.cpp
+++ Helpers.cpp
@@ -1,7 +1,8 @@
 #include "Helpers.h"
 #include <math.h>
 
-double powSum(int a, int b) {
+double powSum(int a, int b) 
+{
 	double exponent = 2;
-	return pow(a + b, exponent);
+	return pow(static_cast<double>(a + b), exponent);
 }
