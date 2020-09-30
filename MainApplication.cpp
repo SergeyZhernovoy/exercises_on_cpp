9c4e4a2f337700de1aed7f7144c84073d62059e3
s.zhernovoy <s_jernov@mail.ru>
2020-10-01 00:22:09 +0300

    lesson 15_4 ready

---
+++ MainApplication.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+#define COUNT 10
+
+void printNumber(int, bool);
+
+
+int main()
+{
+	for (int number = 0; number <= COUNT; number++)
+	{
+		if (number % 2 == 0)
+		{
+			std::cout << number << std::endl;
+		}
+	}
+
+	std::cout << std::endl;
+	std::cout << "print EVEN number from 0 to " << COUNT << std::endl;
+	printNumber(COUNT, true);
+
+	std::cout << std::endl;
+	std::cout << "print ODD number from 0 to " << COUNT << std::endl;
+	printNumber(COUNT, false);
+
+}
+
+
+void printNumber(int counter, bool branch)
+{
+	std::cout << std::endl;
+	for (int number = 0; number <= COUNT; number++) {
+		int EvenOdd = branch == true ? 0 : 1;
+		if (number % 2 == EvenOdd) {
+			std::cout << number << std::endl;
+		}
+	}
+	std::cout << std::endl;
+}
+
